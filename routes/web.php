066fd6b70eb1622df094a38a2e1547212788a61b
gibbo101 <lukegibson101@gmail.com>
2022-10-19 23:40:02 +0100

    play with controllers

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,20 +18,29 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/about', function () {
-    return "Hi about page";
-});
+// Route::get('/about', function () {
+//     return "Hi about page";
+// });
 
 
-Route::get('/contact', function () {
-    return "Hi I am the contact page";
-});
+// Route::get('/contact', function () {
+//     return "Hi I am the contact page";
+// });
 
-Route::get('/post/{id}/{name}', function($id, $name) {
-    return "This is post number ". $id ." ". $name;
-});
+// Route::get('/post/{id}/{name}', function($id, $name) {
+//     return "This is post number ". $id ." ". $name;
+// });
 
-Route::get('admin/posts/example', array('as'=>'admin.home', function() {
-    $url = route('admin.home');
-    return "this url is ". $url;
-}));
+// Route::get('admin/posts/example', array('as'=>'admin.home', function() {
+//     $url = route('admin.home');
+//     return "this url is ". $url;
+// }));
+
+
+ 
+ 
+// Route::get('/', [EdwinsController::class, 'index']); 
+
+// Route::get('/post/{id}', [PostsController::class, 'index']);
+
+Route::resource('posts', PostsController::class);
