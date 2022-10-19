226a166f14c6906dc016051469e3585e8885e8ad
gibbo101 <lukegibson101@gmail.com>
2022-10-19 23:02:06 +0100

    set initial routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,21 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function () {
+    return "Hi about page";
+});
+
+
+Route::get('/contact', function () {
+    return "Hi I am the contact page";
+});
+
+Route::get('/post/{id}/{name}', function($id, $name) {
+    return "This is post number ". $id ." ". $name;
+});
+
+Route::get('admin/posts/example', array('as'=>'admin.home', function() {
+    $url = route('admin.home');
+    return "this url is ". $url;
+}));
